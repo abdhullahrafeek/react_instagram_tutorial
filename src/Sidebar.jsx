c3cf8408517c1a34f5d720e10f807d5a546a12d7
Abdhullah Rafeek <rafeekabdhullah13.work@gmail.com>
2026-02-02 18:42:15 +0800

    Updated to keep the sidebar fixed

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,20 +1,20 @@
 const Sidebar = () => {
     return (
-        <div className="m-3">
+        <div className="m-3 position-fixed">
             <div className="d-flex flex-column gap-2">
                 <img className="logo-text" src="src\assets\Hello-PNG-Image.png" alt="" />
-                <div><i class="bi bi-house-door"></i>Home</div>
-                <div><i class="bi bi-search"></i>Search</div>
-                <div><i class="bi bi-compass"></i>Explore</div>
-                <div><i class="bi bi-camera-reels"></i>Reels</div>
-                <div><i class="bi bi-chat"></i>Messages</div>
-                <div><i class="bi bi-bell"></i>Notifications</div>
-                <div><i class="bi bi-plus-square"></i>Create</div>
-                <div><i class="bi bi-plus-square"></i>Profile</div>
+                <div><i className="bi bi-house-door"></i>Home</div>
+                <div><i className="bi bi-search"></i>Search</div>
+                <div><i className="bi bi-compass"></i>Explore</div>
+                <div><i className="bi bi-camera-reels"></i>Reels</div>
+                <div><i className="bi bi-chat"></i>Messages</div>
+                <div><i className="bi bi-bell"></i>Notifications</div>
+                <div><i className="bi bi-plus-square"></i>Create</div>
+                <div><i className="bi bi-plus-square"></i>Profile</div>
             </div>
             <div className="position-fixed bottom-0 d-flex flex-column gap-2 mb-3">
-                <div><i class="bi bi-threads"></i>Threads</div>
-                <div><i class="bi bi-three-dots"></i>More</div>
+                <div><i className="bi bi-threads"></i>Threads</div>
+                <div><i className="bi bi-three-dots"></i>More</div>
             </div>
         </div>
     )
