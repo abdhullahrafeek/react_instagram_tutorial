54f1ed8137f1a4539e3a1b1675529e85fe2498a3
Abdhullah Rafeek <rafeekabdhullah13.work@gmail.com>
2026-02-02 13:47:51 +0800

    Initial divisions for sidebar, feed and suggestions

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="d-flex vh-100">
+      <div className="w-20">sidebar</div>
+      <div className="w-50">feed</div>
+      <div className="w-30">suggestion</div>
+    </div>
+  );
+}
+
+export default App
