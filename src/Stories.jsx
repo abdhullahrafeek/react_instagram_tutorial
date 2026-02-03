20a94e3c1027066148ac55f38705ea42847d64b1
Abdhullah Rafeek <rafeekabdhullah13.work@gmail.com>
2026-02-03 11:50:07 +0800

    Included onClick in stories

--- src/Stories.jsx
+++ src/Stories.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom";
 
 const Stories = () => {
 
     const [stories, setStories] = useState([]);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetch("http://localhost:3000/stories").
@@ -11,11 +14,13 @@ const Stories = () => {
         catch((err) => console.log(err));
         }, [])
 
+        let tot = stories.length;
+
     return (
         stories.length != 0
         ? <div className="story d-flex">
             {stories.map((story) => (
-                <div className="mx-1" key={story.id}>
+                <div className="mx-1" key={story.id} onClick={() => navigate(`/stories/${story.id}/${tot}`)}>
                     <div className="gradient-border">
                         <img className="story-dp rounded-circle" src={story.user.profile_pic} alt="Profile Picture" />
                     </div>
